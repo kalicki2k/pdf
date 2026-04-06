94777b498cdf82bf3ec3326d918fc43df9436512
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 12:54:01 +0200

    Cover remaining CaretAnnotation branches

--- tests/Document/CaretAnnotationTest.php
+++ tests/Document/CaretAnnotationTest.php
@@ -38,4 +38,20 @@ final class CaretAnnotationTest extends TestCase
 
         new CaretAnnotation(7, $page, 10, 20, 16, 18, symbol: 'Insert');
     }
+
+    #[Test]
+    public function it_uses_the_default_symbol_and_omits_empty_optional_fields(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new CaretAnnotation(7, $page, 10, 20, 16, 18, '', '');
+
+        self::assertSame(
+            "7 0 obj\n"
+            . "<< /Type /Annot /Subtype /Caret /Rect [10 20 26 38] /P 4 0 R /Sy /None >>\n"
+            . "endobj\n",
+            $annotation->render(),
+        );
+        self::assertSame([], $annotation->getRelatedObjects());
+    }
 }
