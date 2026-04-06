24a16a89cbf0fcd90a99fd9d9686e302060298e4
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 12:58:42 +0200

    Cover remaining FreeTextAnnotation branches

--- tests/Document/FreeTextAnnotationTest.php
+++ tests/Document/FreeTextAnnotationTest.php
@@ -40,4 +40,44 @@ final class FreeTextAnnotationTest extends TestCase
             $annotation->render(),
         );
     }
+
+    #[Test]
+    public function it_omits_optional_fields_when_they_are_not_provided(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new FreeTextAnnotation(7, $page, 10, 20, 80, 24, 'Hinweistext', 'F1', 12);
+
+        self::assertSame(
+            "7 0 obj\n"
+            . "<< /Type /Annot /Subtype /FreeText /Rect [10 20 90 44] /P 4 0 R /Contents (Hinweistext) /DA (/F1 12 Tf 0 g) >>\n"
+            . "endobj\n",
+            $annotation->render(),
+        );
+        self::assertSame([], $annotation->getRelatedObjects());
+    }
+
+    #[Test]
+    public function it_renders_cmyk_border_and_fill_colors(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new FreeTextAnnotation(
+            7,
+            $page,
+            10,
+            20,
+            80,
+            24,
+            'Hinweistext',
+            'F1',
+            12,
+            null,
+            Color::cmyk(0.1, 0.2, 0.3, 0.4),
+            Color::cmyk(0.5, 0.6, 0.7, 0.8),
+        );
+
+        self::assertStringContainsString('/C [0.1 0.2 0.3 0.4]', $annotation->render());
+        self::assertStringContainsString('/IC [0.5 0.6 0.7 0.8]', $annotation->render());
+    }
 }
