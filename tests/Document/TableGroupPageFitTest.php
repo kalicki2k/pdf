731d1b468450d864f2feaad3092fdaf201daf158
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 16:46:07 +0200

    Add direct test for TableGroupPageFit

---
+++ tests/Document/TableGroupPageFitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Document;
+
+use Kalle\Pdf\Document\Table\TableGroupPageFit;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class TableGroupPageFitTest extends TestCase
+{
+    #[Test]
+    public function it_stores_group_page_fit_values(): void
+    {
+        $pageFit = new TableGroupPageFit(true, 3);
+
+        self::assertTrue($pageFit->repeatHeaders);
+        self::assertSame(3, $pageFit->fittingRowCountOnCurrentPage);
+    }
+}
