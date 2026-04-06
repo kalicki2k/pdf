5a77159d2d384b0403f7f69a144cf3c70b268e82
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 16:55:29 +0200

    Add direct test for CellRenderResult

---
+++ tests/Document/CellRenderResultTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Document;
+
+use Kalle\Pdf\Document\Document;
+use Kalle\Pdf\Document\Table\Rendering\CellRenderResult;
+use Kalle\Pdf\Document\Text\TextSegment;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class CellRenderResultTest extends TestCase
+{
+    #[Test]
+    public function it_stores_cell_render_result_values(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $remainingLines = [
+            [
+                'segments' => [new TextSegment('continued')],
+                'justify' => false,
+            ],
+        ];
+
+        $result = new CellRenderResult($page, $remainingLines);
+
+        self::assertSame($page, $result->page);
+        self::assertSame($remainingLines, $result->remainingLines);
+    }
+}
