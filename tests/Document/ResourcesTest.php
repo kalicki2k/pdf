682d9907fe52c89fa8b5bc0edeb6cec16e6b1fe8
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 18:29:42 +0200

    Cover remaining Resources branches

--- tests/Document/ResourcesTest.php
+++ tests/Document/ResourcesTest.php
@@ -8,6 +8,7 @@ use Kalle\Pdf\Document\ImageObject;
 use Kalle\Pdf\Document\OptionalContentGroup;
 use Kalle\Pdf\Document\Resources;
 use Kalle\Pdf\Element\Image;
+use Kalle\Pdf\Font\FontDefinition;
 use Kalle\Pdf\Font\StandardFont;
 use Kalle\Pdf\Graphics\Opacity;
 use PHPUnit\Framework\Attributes\Test;
@@ -95,6 +96,16 @@ final class ResourcesTest extends TestCase
     }
 
     #[Test]
+    public function it_reuses_the_existing_image_resource_name_for_the_same_image_id(): void
+    {
+        $resources = new Resources(8);
+        $image = new ImageObject(9, new Image(320, 200, 'DeviceRGB', 'DCTDecode', 'abc123'));
+
+        self::assertSame('Im1', $resources->addImage($image));
+        self::assertSame('Im1', $resources->addImage($image));
+    }
+
+    #[Test]
     public function it_registers_optional_content_groups_as_properties_resources(): void
     {
         $resources = new Resources(8);
@@ -105,5 +116,69 @@ final class ResourcesTest extends TestCase
             "8 0 obj\n<< /Font <<  >> /Properties << /OC1 9 0 R >> >>\nendobj\n",
             $resources->render(),
         );
+    }
+
+    #[Test]
+    public function it_reuses_the_existing_property_resource_name_for_the_same_group_id(): void
+    {
+        $resources = new Resources(8);
+        $group = new OptionalContentGroup(9, 'Notes');
+
+        self::assertSame('OC1', $resources->addProperty($group));
+        self::assertSame('OC1', $resources->addProperty($group));
+    }
+
+    #[Test]
+    public function it_returns_image_objects_including_related_soft_masks(): void
+    {
+        $resources = new Resources(8);
+        $softMask = new ImageObject(10, new Image(320, 200, 'DeviceGray', 'FlateDecode', 'mask'));
+        $image = new ImageObject(9, new Image(320, 200, 'DeviceRGB', 'DCTDecode', 'abc123'), $softMask);
+
+        $resources->addImage($image);
+
+        self::assertSame([$image, $softMask], $resources->getImages());
+    }
+
+    #[Test]
+    public function it_rejects_fonts_that_are_not_indirect_objects(): void
+    {
+        $resources = new Resources(8);
+        $font = new class implements FontDefinition {
+            public function getId(): int
+            {
+                return 1;
+            }
+
+            public function getBaseFont(): string
+            {
+                return 'CustomFont';
+            }
+
+            public function supportsText(string $text): bool
+            {
+                return true;
+            }
+
+            public function encodeText(string $text): string
+            {
+                return $text;
+            }
+
+            public function measureTextWidth(string $text, float $size): float
+            {
+                return 0.0;
+            }
+
+            public function render(): string
+            {
+                return '';
+            }
+        };
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Font resources must be indirect objects.');
+
+        $resources->addFont($font);
     }
 }
