a7fe9684bf661a5eada72819941f17439e18c352
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 13:22:33 +0200

    Cover remaining StrikeOutAnnotation branches

--- tests/Document/StrikeOutAnnotationTest.php
+++ tests/Document/StrikeOutAnnotationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kalle\Pdf\Tests\Document;
 
+use Kalle\Pdf\Document\Annotation\PopupAnnotation;
 use Kalle\Pdf\Document\Annotation\StrikeOutAnnotation;
 use Kalle\Pdf\Document\Document;
 use Kalle\Pdf\Graphics\Color;
@@ -25,5 +26,44 @@ final class StrikeOutAnnotationTest extends TestCase
             . "endobj\n",
             $annotation->render(),
         );
+    }
+
+    #[Test]
+    public function it_omits_optional_fields_when_they_are_not_provided(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new StrikeOutAnnotation(7, $page, 10, 20, 80, 12);
+
+        self::assertSame(
+            "7 0 obj\n"
+            . "<< /Type /Annot /Subtype /StrikeOut /Rect [10 20 90 32] /P 4 0 R /QuadPoints [10 32 90 32 10 20 90 20] >>\n"
+            . "endobj\n",
+            $annotation->render(),
+        );
+        self::assertSame([], $annotation->getRelatedObjects());
+    }
+
+    #[Test]
+    public function it_can_link_a_popup_annotation(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new StrikeOutAnnotation(7, $page, 10, 20, 80, 12, contents: 'Durchgestrichen');
+        $popup = new PopupAnnotation(8, $page, $annotation, 20, 40, 60, 30, true);
+
+        self::assertSame($annotation, $annotation->withPopup($popup));
+        self::assertStringContainsString('/Popup 8 0 R', $annotation->render());
+        self::assertSame([$popup], $annotation->getRelatedObjects());
+    }
+
+    #[Test]
+    public function it_renders_a_cmyk_strike_out_color(): void
+    {
+        $document = new Document(version: 1.4);
+        $page = $document->addPage();
+        $annotation = new StrikeOutAnnotation(7, $page, 10, 20, 80, 12, Color::cmyk(0.1, 0.2, 0.3, 0.4));
+
+        self::assertStringContainsString('/C [0.1 0.2 0.3 0.4]', $annotation->render());
     }
 }
