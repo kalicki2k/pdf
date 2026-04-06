76054f399bce1ac4f09937dfd9cadc033f618571
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 12:38:23 +0200

    Add direct tests for GoToAction

---
+++ tests/Document/Action/GoToActionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Document\Action;
+
+use InvalidArgumentException;
+use Kalle\Pdf\Document\Action\GoToAction;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class GoToActionTest extends TestCase
+{
+    #[Test]
+    public function it_returns_a_pdf_dictionary_for_a_named_destination(): void
+    {
+        $action = new GoToAction('table-demo');
+
+        self::assertSame('<< /S /GoTo /D /table-demo >>', $action->toPdfDictionary()->render());
+    }
+
+    #[Test]
+    public function it_rejects_an_empty_destination(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('GoTo action destination must not be empty.');
+
+        new GoToAction('');
+    }
+}
