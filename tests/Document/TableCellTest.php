10122c10218d65ab47ffee8e3635f354d0efc53b
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-06 16:45:43 +0200

    Add direct test for TableCell

---
+++ tests/Document/TableCellTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Document;
+
+use Kalle\Pdf\Document\Table\Style\CellStyle;
+use Kalle\Pdf\Document\Table\TableCell;
+use Kalle\Pdf\Document\Text\TextSegment;
+use Kalle\Pdf\Layout\HorizontalAlign;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class TableCellTest extends TestCase
+{
+    #[Test]
+    public function it_stores_table_cell_values(): void
+    {
+        $style = new CellStyle(horizontalAlign: HorizontalAlign::CENTER);
+        $segments = [new TextSegment('A'), new TextSegment('B', underline: true)];
+
+        $cell = new TableCell($segments, colspan: 2, rowspan: 3, style: $style);
+
+        self::assertSame($segments, $cell->text);
+        self::assertSame(2, $cell->colspan);
+        self::assertSame(3, $cell->rowspan);
+        self::assertSame($style, $cell->style);
+    }
+}
