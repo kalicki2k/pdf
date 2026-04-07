3c7f24bd7ecb9222f7fffe8d68f9900aebe98bfc
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-07 02:31:32 +0200

    Add direct tests for PermissionBitsResolver

---
+++ tests/Document/PermissionBitsResolverTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Document;
+
+use InvalidArgumentException;
+use Kalle\Pdf\Encryption\EncryptionAlgorithm;
+use Kalle\Pdf\Encryption\EncryptionPermissions;
+use Kalle\Pdf\Encryption\EncryptionProfile;
+use Kalle\Pdf\Encryption\PermissionBitsResolver;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class PermissionBitsResolverTest extends TestCase
+{
+    #[Test]
+    public function it_resolves_permission_bits_for_a_supported_revision(): void
+    {
+        $resolver = new PermissionBitsResolver();
+        $profile = new EncryptionProfile(EncryptionAlgorithm::RC4_128, 128, 2, 3);
+
+        self::assertSame(-4, $resolver->resolve(EncryptionPermissions::readOnly(), $profile));
+    }
+
+    #[Test]
+    public function it_rejects_unsupported_revisions(): void
+    {
+        $resolver = new PermissionBitsResolver();
+        $profile = new EncryptionProfile(EncryptionAlgorithm::RC4_40, 40, 1, 1);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unsupported encryption revision for permission resolution.');
+
+        $resolver->resolve(EncryptionPermissions::readOnly(), $profile);
+    }
+}
