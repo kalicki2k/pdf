e3b9b390129fb471c74572a5beead006ef0e52b9
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-07 03:45:11 +0200

    Add direct test for VerticalAlign

---
+++ tests/Layout/VerticalAlignTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Layout;
+
+use Kalle\Pdf\Layout\VerticalAlign;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class VerticalAlignTest extends TestCase
+{
+    #[Test]
+    public function it_exposes_the_expected_alignment_values(): void
+    {
+        self::assertSame('top', VerticalAlign::TOP->value);
+        self::assertSame('middle', VerticalAlign::MIDDLE->value);
+        self::assertSame('bottom', VerticalAlign::BOTTOM->value);
+    }
+}
