cad59cd72b0a41b0acbde7468d84291c08726d1f
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-07 03:44:24 +0200

    Complete Units coverage

--- tests/Document/UnitsTest.php
+++ tests/Document/UnitsTest.php
@@ -13,6 +13,22 @@ final class UnitsTest extends TestCase
     private const FLOAT_DELTA = 0.0001;
 
     #[Test]
+    public function it_has_a_private_constructor_to_enforce_static_usage(): void
+    {
+        $reflection = new \ReflectionClass(Units::class);
+        $constructor = $reflection->getConstructor();
+
+        self::assertNotNull($constructor);
+        self::assertTrue($constructor->isPrivate());
+
+        $instance = $reflection->newInstanceWithoutConstructor();
+        $constructor->setAccessible(true);
+        $constructor->invoke($instance);
+
+        self::assertInstanceOf(Units::class, $instance);
+    }
+
+    #[Test]
     public function it_converts_millimeters_to_points(): void
     {
         self::assertEqualsWithDelta(56.6929133858, Units::mm(20), self::FLOAT_DELTA);
