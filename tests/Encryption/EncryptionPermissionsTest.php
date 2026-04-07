dc8eccd112626ec3cb48f89933eea05b5b295f5d
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-07 02:35:30 +0200

    Add direct tests for EncryptionPermissions

---
+++ tests/Encryption/EncryptionPermissionsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests\Encryption;
+
+use Kalle\Pdf\Encryption\EncryptionPermissions;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class EncryptionPermissionsTest extends TestCase
+{
+    #[Test]
+    public function it_creates_fully_enabled_permissions(): void
+    {
+        $permissions = EncryptionPermissions::all();
+
+        self::assertTrue($permissions->print);
+        self::assertTrue($permissions->modify);
+        self::assertTrue($permissions->copy);
+        self::assertTrue($permissions->annotate);
+    }
+
+    #[Test]
+    public function it_creates_read_only_permissions(): void
+    {
+        $permissions = EncryptionPermissions::readOnly();
+
+        self::assertFalse($permissions->print);
+        self::assertFalse($permissions->modify);
+        self::assertFalse($permissions->copy);
+        self::assertFalse($permissions->annotate);
+    }
+}
