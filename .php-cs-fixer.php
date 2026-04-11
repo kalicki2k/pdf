a2df8e62c82e58e946580b2a9b3aa058a7db2c14
Sebastian Kalicki <sebastian@kalicki.email>
2026-04-11 11:20:22 +0200

    Add development tooling setup

---
+++ .php-cs-fixer.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->append([__DIR__ . '/example.php']);
+
+return new PhpCsFixer\Config()
+    ->setRiskyAllowed(false)
+    ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
+    ->setRules([
+        '@PSR12' => true,
+        'array_indentation' => true,
+        'blank_line_after_opening_tag' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['return'],
+        ],
+        'cast_spaces' => true,
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'declare_parentheses' => true,
+        'function_declaration' => [
+            'closure_fn_spacing' => 'one',
+        ],
+        'line_ending' => true,
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+        ],
+        'no_extra_blank_lines' => [
+            'tokens' => ['extra'],
+        ],
+        'no_trailing_whitespace' => true,
+        'no_whitespace_in_blank_line' => true,
+        'ordered_imports' => true,
+        'single_blank_line_at_eof' => true,
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arrays', 'arguments', 'parameters'],
+        ],
+        'types_spaces' => [
+            'space' => 'single',
+        ],
+    ])
+    ->setFinder($finder);
---
+++ tests/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kalle\Pdf\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testPhpUnitIsConfigured(): void
+    {
+        self::assertTrue(true);
+    }
+}
